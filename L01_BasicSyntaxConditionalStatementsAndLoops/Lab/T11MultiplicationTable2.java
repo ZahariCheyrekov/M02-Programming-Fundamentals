d613608a3b0713bf45011f1a2436c7a4b58b9382
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-11 15:16:59 +0200

    Update T11MultiplicationTable2.java

--- L01_BasicSyntaxConditionalStatementsAndLoops/Lab/T11MultiplicationTable2.java
+++ L01_BasicSyntaxConditionalStatementsAndLoops/Lab/T11MultiplicationTable2.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class T11MultiplicationTable2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+    
         int firstNumber = Integer.parseInt(scanner.nextLine());
         int secondNumber = Integer.parseInt(scanner.nextLine());
 
