1a9f94f1a6307e4debba50e656df686f959bbbc8
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-21 17:03:10 +0200

    Update T06MiddleCharacters.java

--- L04_Methods/Exercises/T06MiddleCharacters.java
+++ L04_Methods/Exercises/T06MiddleCharacters.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class T06MiddleCharacters {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+       
         String input = scanner.nextLine();
+        
         String result = printMiddlePart(input);
         System.out.println(result);
     }
